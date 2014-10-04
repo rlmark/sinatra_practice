95c61b99d1541931f5a9f4300b444297d413eada
Rebecca Mark <rebecca.lmark@gmail.com>
2014-10-04 15:09:24 -0700

    Trying fade in on first view of homepage function

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,14 @@
 $(document).ready( function () {
 
-    $('.longtext').readmore();
+  if ( document.referrer === null || document.referrer.indexOf(window.location.hostname) < 0 ) {
+    $('.bigtext').fadeIn("slow");
+};
 
-    $(window).scroll( function () {
-      $("footer").fadeIn("slow");
-    });
+  $('.longtext').readmore();
+
+  $(window).scroll( function () {
+    $("footer").fadeIn("slow");
+  });
 
   // PRACTICE
   //   $("h2").css({color: "red"});
@@ -14,9 +18,9 @@ $(document).ready( function () {
   //    $(this).css("font-size", "1em");
   //  });
 
-    $('a[href^="#"]').click(function (){
-      $('html, body').animate({scrollTop: $( $.attr(this, 'href') ).offset().top}, 900);
+  $('a[href^="#"]').click(function (){
+    $('html, body').animate({scrollTop: $( $.attr(this, 'href') ).offset().top}, 900);
       return false;
-    });
+  });
 
 });

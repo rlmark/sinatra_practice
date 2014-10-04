6524e1ef79db8fda0c3bf46b5cabf22830137c38
Rebecca Mark <rebecca.lmark@gmail.com>
2014-10-04 16:03:41 -0700

    working css animations when hovering over icons.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,16 +1,28 @@
 $(document).ready( function () {
 
+  //Fades in counter only on first visit to site, else no animation
   if ( document.referrer == null || document.referrer.indexOf(window.location.hostname) < 0 ) {
     $('.bigtext').fadeIn("slow");
 }
   else
     $('.bigtext').css({display: "block"});
 
+  //Clips posts and allows user expand posts
   $('.longtext').readmore();
 
+  //Fade in footer
   $(window).scroll( function () {
     $("footer").fadeIn("slow");
   });
+
+  //All internal links get scroll animation
+  $('a[href^="#"]').click(function (){
+    $('html, body').animate({scrollTop: $( $.attr(this, 'href') ).offset().top}, 900);
+      return false;
+  });
+
+});
+
 
   // PRACTICE
   //   $("h2").css({color: "red"});
@@ -19,10 +31,3 @@ $(document).ready( function () {
   //     $(this).css({color: "red"});
   //    $(this).css("font-size", "1em");
   //  });
-
-  $('a[href^="#"]').click(function (){
-    $('html, body').animate({scrollTop: $( $.attr(this, 'href') ).offset().top}, 900);
-      return false;
-  });
-
-});
